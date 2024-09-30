70e08540d2583209c4668a30938d939ab7af12e7
vahidseyyedi <132602407+vahidseyyedi@users.noreply.github.com>
2024-09-30 15:48:41 +0330

    .

---
+++ 01/1.ino
@@ -0,0 +1,14 @@
+//vahid seyyedi
+//Session 1
+
+int led = 13;
+void setup() {
+pinMode(led, OUTPUT);
+}
+
+void loop() {
+digitalWrite(led, HIGH);
+delay(2000);
+digitalWrite(led, LOW);
+delay(2000);
+}
